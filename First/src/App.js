8544a612ecd5cd456d96b0f8265b5669e4611672
Abdallh Alhelel <abdallh20hl@gmail.com>
2026-08-04 19:39:29 +0300

    uisng arrays in use state

--- First/src/App.js
+++ First/src/App.js
@@ -5,16 +5,40 @@ import Header from "./Header";
 import MyForm from "./Myform";
 import Post from "./Post";
 
+import { useState } from "react";
 import MyInput from "./myinput.js";
 
 function App() {
-  const devices = ["iphone", "andriod", "samsung", "windows"];
+  const [DeivceNameToaddinput, SetDeivceNameToadd] = useState("");
+
+  const [devices, Setdevices] = useState([
+    "iphone",
+    "andriod",
+    "samsung",
+    "windows",
+  ]);
+
   const deviceslist = devices.map((item) => {
     return <li>{item} </li>;
   });
+
+  function handelClieck() {
+    const newarray = [...devices];
+    debugger;
+    newarray.push(DeivceNameToaddinput);
+    Setdevices(newarray);
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={{ marginTop: "10px", fontSize: "1rem" }}>
       <ul>{deviceslist}</ul>
+      <input
+        value={DeivceNameToaddinput}
+        onChange={(event) => {
+          SetDeivceNameToadd(event.target.value);
+        }}
+      ></input>
+      <button onClick={handelClieck}>add </button>
     </div>
   );
 }
