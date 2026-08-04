f0d2f9388c34c9497de1894b69cf2da44a727896
Abdallh Alhelel <abdallh20hl@gmail.com>
2026-08-04 20:17:47 +0300

    use edit and delete

--- First/src/App.js
+++ First/src/App.js
@@ -7,26 +7,52 @@ import Post from "./Post";
 
 import { useState } from "react";
 import MyInput from "./myinput.js";
-
+let counter = 5;
 function App() {
   const [DeivceNameToaddinput, SetDeivceNameToadd] = useState("");
 
   const [devices, Setdevices] = useState([
-    "iphone",
-    "andriod",
-    "samsung",
-    "windows",
+    { id: 1, Name: "iphone" },
+    { id: 2, Name: "andriod" },
+    { id: 3, Name: "samsung" },
+    { id: 4, Name: "windows" },
   ]);
+  function handelDaleteitem(id) {
+    const newarray = [...devices];
+
+    console.log("length" + newarray.length);
+
+    const delleteitem = newarray.filter((x) => x.id != id);
+
+    alert(id);
+    Setdevices([...delleteitem]);
+  }
 
   const deviceslist = devices.map((item) => {
-    return <li>{item} </li>;
+    return (
+      <li key={item.id}>
+        {item.Name}{" "}
+        <button
+          onClick={() => {
+            handelDaleteitem(item.id);
+          }}
+        >
+          {" "}
+          Delete{" "}
+        </button>
+      </li>
+    );
   });
 
-  function handelClieck() {
-    const newarray = [...devices];
-    debugger;
-    newarray.push(DeivceNameToaddinput);
-    Setdevices(newarray);
+  function handelClieck(id) {
+    // const newarray = [...devices];
+    // debugger;
+    // console.log("length" + newarray.length);
+    // newarray.push(DeivceNameToaddinput);
+    Setdevices([...devices, { id: counter, Name: DeivceNameToaddinput }]);
+    counter = counter + 1;
+
+    console.info([...devices]);
   }
 
   return (
