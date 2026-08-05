9e7ba7236c4fc90a6099205307be2388b9592297
Abdullah Alhelal <119299498+AbdullahAlhelal@users.noreply.github.com>
2026-08-05 12:11:58 +0300

    use state with serveral Time

--- First/src/App.js
+++ First/src/App.js
@@ -1,64 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MyFirstCommpnant from "./MyFirstCommpnant";
-import Header from "./Header";
-import MyForm from "./Myform";
-import Post from "./Post";
 
 import { useState } from "react";
-import MyInput from "./myinput.js";
-let counter = 5;
+
+let counter = 0;
 function App() {
-  const [DeivceNameToaddinput, SetDeivceNameToadd] = useState("");
 
-  const [devices, Setdevices] = useState([
-    { id: 1, name: "iphone" },
-    { id: 2, name: "andriod" },
-    { id: 3, name: "samsung" },
-    { id: 4, name: "windows" },
-  ]);
-  function HandelDeleteItem(id) {
-    const itemWithoutDelete = devices.filter((x) => x.id !== id);
-    Setdevices(itemWithoutDelete);
+  function HandelCountPlus() {
+    // setcounter(counter + 1);
+    // setcounter(counter + 1);
+
+    setcounter((x) => x + 1);
+    setcounter((x) => x + 1);
 
   }
 
-  function HandeleditItem(id) {
-    const newarray = devices.map((device) => {
-      if (device.id === id) {
-        let newdevice = { ...device, name: DeivceNameToaddinput }
-        return newdevice;
-      } else
-        return device;
-    });
-    // debugger;
-    // newarray.push(DeivceNameToaddinput);
-    Setdevices(newarray);
-
-  }
-  const deviceslist = devices.map((item) => {
-    return <li key={item.id} >{item.name} <button onClick={() => HandelDeleteItem(item.id)}> delete</button>
-      <button onClick={() => HandeleditItem(item.id)}> edit</button></li>;
-  });
-
-  function handelClieck() {
-    // const newarray = [...devices];
-    // debugger;
-    // newarray.push(DeivceNameToaddinput);
-    // Setdevices(newarray);
-    Setdevices([...devices, { id: counter, name: DeivceNameToaddinput }]);
-  }
-
+  let [counter, setcounter] = useState(0)
   return (
     <div className="App" style={{ marginTop: "10px", fontSize: "1rem" }}>
-      <ul>{deviceslist}</ul>
-      <input
-        value={DeivceNameToaddinput}
-        onChange={(event) => {
-          SetDeivceNameToadd(event.target.value);
-        }}
-      ></input>
-      <button onClick={handelClieck}>add </button>
+      <h1> the count  {counter}</h1>
+      <button onClick={HandelCountPlus}>+</button>
     </div>
   );
 }
