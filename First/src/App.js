d0efd050baca7d24b511ccb9fe2392c63abd60c3
Abdullah Alhelal <119299498+AbdullahAlhelal@users.noreply.github.com>
2026-08-05 10:59:57 +0300

    use edit  item with array

--- First/src/App.js
+++ First/src/App.js
@@ -7,26 +7,46 @@ import Post from "./Post";
 
 import { useState } from "react";
 import MyInput from "./myinput.js";
-
+let counter = 5;
 function App() {
   const [DeivceNameToaddinput, SetDeivceNameToadd] = useState("");
 
   const [devices, Setdevices] = useState([
-    "iphone",
-    "andriod",
-    "samsung",
-    "windows",
+    { id: 1, name: "iphone" },
+    { id: 2, name: "andriod" },
+    { id: 3, name: "samsung" },
+    { id: 4, name: "windows" },
   ]);
+  function HandelDeleteItem(id) {
+    const itemWithoutDelete = devices.filter((x) => x.id !== id);
+    Setdevices(itemWithoutDelete);
 
+  }
+
+  function HandeleditItem(id) {
+    const newarray = devices.map((device) => {
+      if (device.id === id) {
+        let newdevice = { ...device, name: DeivceNameToaddinput }
+        return newdevice;
+      } else
+        return device;
+    });
+    // debugger;
+    // newarray.push(DeivceNameToaddinput);
+    Setdevices(newarray);
+
+  }
   const deviceslist = devices.map((item) => {
-    return <li>{item} </li>;
+    return <li key={item.id} >{item.name} <button onClick={() => HandelDeleteItem(item.id)}> delete</button>
+      <button onClick={() => HandeleditItem(item.id)}> edit</button></li>;
   });
 
   function handelClieck() {
-    const newarray = [...devices];
-    debugger;
-    newarray.push(DeivceNameToaddinput);
-    Setdevices(newarray);
+    // const newarray = [...devices];
+    // debugger;
+    // newarray.push(DeivceNameToaddinput);
+    // Setdevices(newarray);
+    Setdevices([...devices, { id: counter, name: DeivceNameToaddinput }]);
   }
 
   return (
