2895f6687077d3aab94f87f555064dfa7b073f40
Abdullah Alhelal <119299498+AbdullahAlhelal@users.noreply.github.com>
2026-08-06 16:04:01 +0300

    material mu

---
+++ React-material-ui/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Material from "./Material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { orange, purple } from "@mui/material/colors";
+
+const theme = createTheme({
+  status: {
+    danger: orange[500],
+  },
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+  }
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App" style={{ marginTop: "200px", marginLeft: "500px" }}>
+        <Material />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
